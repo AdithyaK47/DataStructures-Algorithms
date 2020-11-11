5a2bf3251d61aa46652bbfd4842417b9e5e92942
Adithya Krishna <68055622+AdithyaK47@users.noreply.github.com>
2020-11-11 12:39:08 +0530

    Create Basics.c

---
+++ Doubly Linked List/Basics.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node  {
+	int data;
+	struct Node* next;
+	struct Node* prev;
+};
+
+struct Node* head; // global variable - pointer to head node.
+
+//Creates a new Node and returns pointer to it. 
+struct Node* GetNewNode(int x) {
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	newNode->data = x;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return newNode;
+}
